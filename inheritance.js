7e0f65116b188aa38871a8881e033d3d2921683f
julian <juka1031@gmail.com>
2021-08-06 16:50:02 -0400

    finished

---
+++ inheritance.js
@@ -0,0 +1,36 @@
+//Preferred way!
+Function.prototype.inherits = function (parentClass) {
+    let childClass = this;
+    function Surrogate() {}
+    Surrogate.prototype = parentClass.prototype;
+    childClass.prototype = new Surrogate(); //when we create a new instance of Surrogate we make an object who's __proto__ points to Surrogate's prototype(orangebox) "the connector"
+    childClass.prototype.constructor = childClass; //overriding the child classes prototype so that it constructor points back to its original constructor
+};
+
+
+function Animal(name) {
+    this.name = name;
+  }
+
+  
+Animal.prototype.eat = function () {
+    console.log(this.name + " is eating.");
+  };
+
+  
+function Cat(name) {
+    this.name = name;
+  }
+
+  Cat.inherits(Animal);
+
+Cat.prototype.meow = function () {
+    console.log("Meow!");
+};
+
+const garfield = new Cat("Garfield");
+garfield.eat();
+garfield.meow();
+
+// const noCat = new Animal("noCat");
+// noCat.meow();
